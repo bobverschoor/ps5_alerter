0aee58b482411bc555828f49f94ef2cd1fe64757
bob <bob.verschoor@alliander.com>
2020-12-19 12:17:24 +0100

    Meerdere winkels toegevoegd

--- src/main/python/ps5_main.py
+++ src/main/python/ps5_main.py
@@ -4,16 +4,20 @@ import os
 
 BASE_URL = "https://api.telegram.org/bot"
 BOL_COM_PS5_URL = 'https://www.bol.com/nl/p/sony-playstation-5-console/9300000004162282'
-CONFIG_FILE = 'resource/secrets.txt'
+SECRETS_FILE = 'resource/secrets.txt'
+WINKELS_FILE = 'resource/winkels.txt'
 TELEGRAM = 'telegram'
 CHANNEL_ID = 'channel_id'
 TOKEN = 'TOKEN'
+WINKEL = 'winkel'
+VOORRAAD_TEKST = 'voorraad_tekst'
+URL = 'url'
 
 
-def get_config():
+def get_secrets():
     config = configparser.ConfigParser()
-    if os.path.isfile(CONFIG_FILE):
-        config.read(CONFIG_FILE)
+    if os.path.isfile(SECRETS_FILE):
+        config.read(SECRETS_FILE)
         if TELEGRAM in config:
             if CHANNEL_ID in config['telegram'] and TOKEN in config[TELEGRAM]:
                 return config
@@ -26,26 +30,44 @@ def get_config():
     exit(1)
 
 
-def leverbaar():
-    r = requests.get(BOL_COM_PS5_URL)
+def get_winkels():
+    winkels = configparser.ConfigParser()
+    if os.path.isfile(WINKELS_FILE):
+        winkels.read(WINKELS_FILE)
+    return winkels
+
+
+def leverbaar(url, check):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1)'
+    }
+    #amazon redirects indien geen browser like user agent
+    r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        if 'Niet leverbaar' in r.text:
+        if check in r.text:
             return False
-    return True
+        else:
+            print(r.text)
+            return True
+    else:
+        print(r.status_code)
+        print(url)
+        return False
 
 
 def main():
-    if not leverbaar():
-        config = get_config()
-        message = {'chat_id': "-100" + config[TELEGRAM][CHANNEL_ID],
-                   'text': 'bol.com\n'
-                           'https://www.bol.com/nl/p/sony-playstation-5-console/9300000004162282'}
-        r = requests.post(BASE_URL + config[TELEGRAM][TOKEN] + "/sendMessage", data=message)
-        if r.status_code != 200:
-            print(r.text)
-            exit(1)
-    else:
-        print("niet leverbaar")
+    winkel = get_winkels()
+    for winkelnaam in winkel.sections():
+        if leverbaar(winkel[winkelnaam][URL], winkel[winkelnaam][VOORRAAD_TEKST]):
+            secret = get_secrets()
+            message = {'chat_id': "-100" + secret[TELEGRAM][CHANNEL_ID],
+                       'text': winkelnaam + '\n' + winkel[winkelnaam][URL]}
+            r = requests.post(BASE_URL + secret[TELEGRAM][TOKEN] + "/sendMessage", data=message)
+            if r.status_code != 200:
+                print(r.text)
+                exit(1)
+        else:
+            print("niet leverbaar bij " + winkelnaam)
 
 
 if __name__ == "__main__":
